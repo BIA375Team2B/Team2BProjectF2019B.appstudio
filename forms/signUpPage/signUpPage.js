8a943e1bdd2ee4b44d6fadf288df4cdd53296a48
Lizzie <ekr74869@creighton.edu>
2019-11-13 15:27:48 -0600

    Updates

--- forms/signUpPage/signUpPage.js
+++ forms/signUpPage/signUpPage.js
@@ -1,13 +1,12 @@
 
-let accountInfo = []
-
 btnAddAccount.onclick=function(){
   let queryAdd = "INSERT INTO students VALUES("inptFirstName.value", "inptLastName.value", "inptEmail.value", "inptGradYear.value", "inptMajor.value")";
   req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
   if (req1.status == 200) {
-    if (req1.status == 500)
-     alert("It's been successfully added!")
-   else 
+    if (req1.status == 500){
+     alert("You're account has been added!")
+     ChangeForm(homeScreen)
+   } else 
      alert("Error: It wasn't able to be added.")
    else
      alert("Error: " + req1.status)
