80bde7032439c52f9cfe3d5d40368fc00670e12d
Lizzie <ekr74869@creighton.edu>
2019-11-13 16:08:33 -0600

    Updates

--- forms/loginPage/loginPage.js
+++ forms/loginPage/loginPage.js
@@ -14,16 +14,3 @@ btnSignIn.onclick=function(){
     }
 }
 
-btnSignUp.onclick=function(){
-    req1 = Ajax("https://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptUser.value + "&j_password=" + inptPass.value);
-    if (req1.status == 200) { 
-      if (req1.responseText == 1)
-        ChangeForm(signUpPage)
-      else
-        NSB.MsgBox("Sorry, that is not valid login information.")
-        //1 good 0 bad
-        
-    } else {
-      NSB.MsgBox("Error Connection Not Made: " + req1.status + " readystate " + req1.readyState + " status text " + req1.statusText)
-    }
-}
--- forms/signUpPage/signUpPage.js
+++ forms/signUpPage/signUpPage.js
@@ -3,11 +3,12 @@ btnAddAccount.onclick=function(){
   let queryAdd = "INSERT INTO students VALUES("inptFirstName.value", "inptLastName.value", "inptEmail.value", "inptGradYear.value", "inptMajor.value")";
   req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
   if (req1.status == 200) {
-    if (req1.status == 500){
+    if (req1.status == 500) {
      alert("You're account has been added!")
-     ChangeForm(homeScreen)
+     ChangeForm(HomeScreen)
    } else 
-     alert("Error: It wasn't able to be added.")
+     alert("Error: Your information wasn't able to be added.")
    else
      alert("Error: " + req1.status)
+  }
 }
