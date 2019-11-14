30e79e685ad20348f44841c4e2fb2c8ce86d6e7e
Lizzie <ekr74869@creighton.edu>
2019-11-13 18:00:10 -0600

    Changes

--- forms/loginPage/loginPage.js
+++ forms/loginPage/loginPage.js
@@ -1,16 +1,25 @@
 
+let arrayNetId = []
+
 btnSignIn.onclick=function(){
-    
+    var query = "SELECT NetID FROM students WHERE NetID= " + '"' + inptUser.value + '"'
+    req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=375groupb2&query=" + query);
+    if (req2.status == 200) {
+        arrayNetId = JSON.parse(req2.responseText)
+    } else 
+      NSB.MsgBox("Error: " + req2.status)
+  
+  //check that the user is a student
     req1 = Ajax("https://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptUser.value + "&j_password=" + inptPass.value);
     if (req1.status == 200) { 
-      if (req1.responseText == 1)
-        ChangeForm(HomeScreen)
-      else
-        NSB.MsgBox("Sorry, that is not valid login information.")
+        lblWelcome.style.display = "block"  // none to hide
+        lblResult.style.display = "block"  // none to hide
+        lblResult.value = "The authentication code is " + req1.responseText
         //1 good 0 bad
         
     } else {
-      NSB.MsgBox("Error Connection Not Made: " + req1.status + " readystate " + req1.readyState + " status text " + req1.statusText)
+        //Handle that. 
+        lblResult.style.display = "block"  // none to hide
+        lblResult.value = "Error Connection Not Made: " + req1.status + " readystate " + req1.readyState + " status text " + req1.statusText;
     }
 }
-
--- forms/signUpPage/signUpPage.js
+++ forms/signUpPage/signUpPage.js
@@ -1,10 +1,11 @@
 
 btnAddAccount.onclick=function(){
-  let queryAdd = "INSERT INTO students VALUES("inptFirstName.value", "inptLastName.value", "inptEmail.value", "inptGradYear.value", "inptMajor.value")";
-  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=ekr74869&query=" + query)
+  let queryAdd = "INSERT INTO students VALUES("inptNetID.value", "inptFirstName.value", "inptLastName.value", "inptEmail.value", "inptGradYear.value", "inptMajor.value")";
+  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=375groupb2&query=" + query)
   if (req1.status == 200) {
     if (req1.status == 500) {
-     alert("You're account has been added!")
+      var result = JSON.parse(req1.responseText)
+      NSB.MsgBox("You have successfully added your account!")
      ChangeForm(HomeScreen)
    } else 
      alert("Error: Your information wasn't able to be added.")
