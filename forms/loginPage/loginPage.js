4ff7b6009ab589dcc0800ddcf303703ee3a65eeb
Lizzie <ekr74869@creighton.edu>
2019-11-18 16:22:14 -0600

    Change

--- forms/loginPage/loginPage.js
+++ forms/loginPage/loginPage.js
@@ -1,17 +1,24 @@
 /*
-let arrayNetId = []
+let arrayResults = []
 
 btnSignIn.onclick=function(){
-    var query = "SELECT NetID FROM students WHERE NetID= " + '"' + inptUser.value + '"'
+    var query = "SELECT NetID, password FROM students WHERE NetID= " + '"' + inptUser.value + '"' AND password= " + '"' + inptPass.value + '"'
     req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=375groupb2&query=" + query);
     if (req1.status == 200) {
-        arrayNetId = JSON.parse(req1.responseText)
-        if (arrayNetId.length == 0) {
+        arrayResults = JSON.parse(req1.responseText)
+        if (arrayResults.length == 0) {
             NSB.MsgBox("Sorry, you're not in our system. Sign up to be added.")
             ChangeForm(signUpPage)
-        } else 
+        } else if (arrayResults.length == 2) 
             ChangeForm(HomeScreen)
+          else 
+            NSB.MsgBox("Error: Sorry, your account is not available.")
     } else 
       NSB.MsgBox("Error: " + req1.status)
+}
+*/
+/*
+btnSignUp.onclick=function(){
+  ChangeForm(signUpPage)
 }
 */
