c4294fd5082657744a225121ee9d441da4b4f744
Lizzie <ekr74869@creighton.edu>
2019-11-20 15:56:38 -0600

    updates

---
+++ forms/myAccountPage/myAccountPage.js
@@ -0,0 +1,9 @@
+
+myAccountPage.onshow=function(){
+//show current date
+  let wholeCurrentDate = String(new Date());
+  let currentDate = wholeCurrentDate.substring(0,21);
+  lblCurTime.value = currentDate;
+  
+  var
+}
