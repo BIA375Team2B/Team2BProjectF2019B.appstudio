79be3dbe4f06a72937882c47833fe851e6dbf7b7
Lizzie <ekr74869@creighton.edu>
2019-11-20 17:01:44 -0600

    h

--- forms/myAccountPage/myAccountPage.js
+++ forms/myAccountPage/myAccountPage.js
@@ -5,5 +5,7 @@ myAccountPage.onshow=function(){
   let currentDate = wholeCurrentDate.substring(0,21);
   lblCurTime.value = currentDate;
   
-  var
+//show current name
+  var queryName = S
+  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=375groupb2&query=" + queryName)
 }
--- forms/signUpPage/signUpPage.js
+++ forms/signUpPage/signUpPage.js
@@ -1,7 +1,6 @@
 /*
 btnAddAccount.onclick=function(){
   let queryAdd = "INSERT INTO students VALUES('"+inptNetID.value+"', '"+inptFirstName.value+"', '"+inptLastName.value+"', '"+inptEmail.value+"', "+inptGradYear.value+", '"+inptMajor.value+"', '"+inptPass1.value+"')";
-  console.log(queryAdd)
   req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekr74869&pass=Boolarina18%&database=375groupb2&query=" + queryAdd)
   if (req1.status == 200) {
     if (req1.responseText == 500) {
